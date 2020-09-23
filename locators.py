df8aa5131b6b784b034f831d8266c10f69e199a0
moonzaki <71381734+moonzaki@users.noreply.github.com>
2020-09-23 03:50:52 +0300

    Delete locators.py

--- locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class SearchPageLocators():
-    SEARCH_FORM = (By.CSS_SELECTOR, "#text")
-    SUGG_FORM = (By.CSS_SELECTOR, ".mini-suggest__popup")
-    SEARCH_RESULTS = (By.CSS_SELECTOR, ".main__content")
-    SEARCH_LINK = (By.CSS_SELECTOR, ".link.organic__url")
